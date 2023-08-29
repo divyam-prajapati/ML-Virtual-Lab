46f121b1772d8c593b2aea78ce7fe8dfff798a8e
Taha-001 <58072544+Taha-001@users.noreply.github.com>
2023-08-29 10:03:06 +0530

    Update 4_Random_Forest.py

--- pages/4_Random_Forest.py
+++ pages/4_Random_Forest.py
@@ -3,7 +3,7 @@ import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.datasets import load_diabetes, load_boston
+from sklearn.datasets import load_diabetes
 import matplotlib.pyplot as plt
 
 
